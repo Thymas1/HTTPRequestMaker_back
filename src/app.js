a5a4c6a868a34bdcc070de3dda82989139078ee8
Thomase <thomase@stacc.com>
2022-01-08 00:40:29 +0100

    feat: init commit

---
+++ src/app.js
@@ -0,0 +1,34 @@
+const express = require("express");
+
+
+const request = require("./routes/request");
+
+
+const allowCORSMiddleware = (req, res, next) => {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Credentials", "true");
+	res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
+	res.header(
+		"Access-Control-Expose-Headers",
+		"Content-Length, Content-Type, Content-Disposition"
+	);
+	res.header(
+		"Access-Control-Allow-Headers",
+		"Accept, Authorization, Content-Type, X-Requested-With, Range"
+	);
+	if (req.method === "OPTIONS") {
+		return res.sendStatus(200);
+	} else {
+		return next();
+	}
+};
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+
+app.use("/request", request);
+
+module.exports = app;
---
+++ src/routes/request.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const router = express.Router();
+const axios = require("axios")
+const get = require("lodash.get")
+
+const axiosClient = axios.create({
+	timeout: 60000,
+})
+axiosClient.interceptors.request.use( x => {
+	// to avoid overwriting if another interceptor
+	// already defined the same object (meta)
+	x.meta = x.meta || {}
+	x.meta.requestStartedAt = new Date().getTime();
+	return x;
+})
+axiosClient.interceptors.response.use( x => {
+	x.responseTime = new Date().getTime() - x.config.meta.requestStartedAt;
+	return x;
+})
+
+
+
+const testRequest = async (req, res) => {
+return res.status(200).send("Running")
+};
+const getMethod = async (req, res) => {
+	const {url} = get(req, "query")
+	console.log(url);
+	try {
+		const request = await axiosClient.get(url)
+		const body = request.data
+		const status = request.status
+		const statusText = request.statusText
+		const headers = request.headers
+		const responseTime = request.responseTime
+		return res.status(200).send({"body": body, "status": status, "statusText": statusText, "headers": headers, "responseTimeInMs": responseTime})
+	}catch (e) {
+		res.status(500).send(e.message)
+	}
+};
+
+router.get("/", testRequest);
+router.get("/getMethod", getMethod);
+
+module.exports = router;
