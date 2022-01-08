c0e5a4c0128aaa5d68c8f3010f2716feef31c186
Thomase <thomase@stacc.com>
2022-01-08 02:26:55 +0100

    started

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 const express = require("express");
-
+var cors = require('cors')
 
 const request = require("./routes/request");
 
@@ -29,6 +29,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 
+app.use(allowCORSMiddleware);
+app.use(cors())
 app.use("/request", request);
 
 module.exports = app;
--- src/routes/request.js
+++ src/routes/request.js
@@ -39,7 +39,28 @@ const getMethod = async (req, res) => {
 	}
 };
 
+const postMethod = async (req, res) => {
+	const {url} = get(req, "query")
+	console.log(url);
+	console.log("REQ BODY", req.body);
+	try {
+		const request = await axiosClient.post(url, req.body)
+		const body = request.data
+		const status = request.status
+		const statusText = request.statusText
+		const headers = request.headers
+		const responseTime = request.responseTime
+		return res.status(200).send({"body": body, "status": status, "statusText": statusText, "headers": headers, "responseTimeInMs": responseTime})
+	}catch (e) {
+		res.status(500).send(e.message)
+	}
+};
+
+
 router.get("/", testRequest);
 router.get("/getMethod", getMethod);
+router.post("/postMethod", postMethod)
 
 module.exports = router;
+
+
